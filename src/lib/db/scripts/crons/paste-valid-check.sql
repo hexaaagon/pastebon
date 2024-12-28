fc6e8e07467233f0316cba81757b957dd23d9559
scoooolz7657 <scoooolz7657@gmail.com>
2024-12-28 14:16:29 +0000

    fix(cron): fixes http call function

--- src/lib/db/scripts/crons/paste-valid-check.sql
+++ src/lib/db/scripts/crons/paste-valid-check.sql
@@ -3,5 +3,5 @@ select
   cron.schedule (
     'paste-valid-check',
     '0 */6 * * *',
-    '/* This Cron job will check if any paste is valid */ CALL net.http_post(url := ''{{APP_URL}}/api/server/paste-valid-check'', headers := JSONB_BUILD_OBJECT(''Authorization'', ''{{REST_API_PASSWORD}}'') /* Don''t share this Authorization Key. */, timeout_milliseconds := 10000);'
+    '/* This Cron job will check if any paste is valid */ SELECT net.http_post(url := ''{{APP_URL}}/api/server/paste-valid-check'', headers := JSONB_BUILD_OBJECT(''Authorization'', ''{{REST_API_PASSWORD}}'') /* Don''t share this Authorization Key. */, timeout_milliseconds := 10000);'
   );
