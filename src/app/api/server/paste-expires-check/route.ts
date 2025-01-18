0053d3f6218c2e0b55fec6c6f3a8e8be3c19cac6
Hexaa <128217934+hexaaagon@users.noreply.github.com>
2025-01-18 10:53:50 +0000

    feat(paste): implement expiration check and synchronization with storage

--- src/app/api/server/paste-expires-check/route.ts
+++ src/app/api/server/paste-expires-check/route.ts
@@ -1,10 +1,25 @@
 import { checkAuthorizationKey } from "@/lib/actions/authorization";
+import { createServiceServer } from "@/lib/supabase/service-server";
 
 export async function POST(req: Request) {
   const authorizationKey = req.headers.get("Authorization");
   const check = await checkAuthorizationKey(authorizationKey);
 
   if (!check.success) return Response.json(check, { status: 401 });
+
+  const supabase = createServiceServer();
+  const pasteDatabase = await supabase
+    .from("paste")
+    .select("*")
+    .lte("expires_at", new Date().toISOString());
+
+  if (pasteDatabase.error) return Response.json({ success: false });
+
+  if (pasteDatabase.data.length > 0) {
+    for (const paste of pasteDatabase.data) {
+      await supabase.storage.from("paste").remove([paste.path]);
+    }
+  }
 
   return Response.json({ success: true });
 }
--- src/app/api/server/paste-valid-check/route.ts
+++ src/app/api/server/paste-valid-check/route.ts
@@ -1,10 +1,37 @@
 import { checkAuthorizationKey } from "@/lib/actions/authorization";
+import { createServiceServer } from "@/lib/supabase/service-server";
 
 export async function POST(req: Request) {
   const authorizationKey = req.headers.get("Authorization");
   const check = await checkAuthorizationKey(authorizationKey);
 
   if (!check.success) return Response.json(check, { status: 401 });
+
+  const supabase = createServiceServer();
+  const pasteDatabase = await supabase.from("paste").select("*");
+
+  if (pasteDatabase.error) return Response.json({ success: false });
+
+  const storage = supabase.storage.from("paste");
+  const storageList = await storage.list();
+
+  if (storageList.error) return Response.json({ success: false });
+
+  const databaseIds = pasteDatabase.data.map((paste) => paste.id);
+  const storageIds = storageList.data.map((file) => file.name.split(".")[0]);
+
+  const missingInDatabase = storageIds.filter(
+    (id) => !databaseIds.includes(id),
+  );
+  const missingInStorage = databaseIds.filter((id) => !storageIds.includes(id));
+
+  for (const id of missingInDatabase) {
+    await storage.remove([`${id}.txt`]);
+  }
+
+  for (const id of missingInStorage) {
+    await supabase.from("paste").delete().eq("id", id);
+  }
 
   return Response.json({ success: true });
 }
